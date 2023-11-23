21df2c076d314976ebc9f3399c5b53a3b8c58b2d
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-23 14:57:10 +0530

    Add contact page and contact form component

---
+++ components/ContactForm.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ContactForm = () => {
+    return(
+        <div className="flex flex-col w-full h-9 bg-gray">
+            <div className="flex">
+                <h2 className="uppercase text-white">Got an idea?</h2>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default ContactForm;
