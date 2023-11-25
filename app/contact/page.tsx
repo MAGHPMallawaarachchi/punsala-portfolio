6719bb5addbf9666db1fa181f05d307ed0d71a4b
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-25 09:49:38 +0530

    Refactor contact page component name

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 import ContactForm from '@/components/ContactForm'
 import React from 'react';
 
-export default function Contact(){
+export default function ContactPage(){
     return(
         <section className='mx-auto 3xl:max-w-7xl max-w-5xl flex items-center h-[100%]'>
             <ContactForm />
