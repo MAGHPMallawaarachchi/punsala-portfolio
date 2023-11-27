367cf6a38769498afa3dc218a6d017d4451e6db4
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-27 22:55:47 +0530

    Update Footer component with logo and navigation links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,30 @@
+import { NavLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
     return(
-        <footer className="flex justify-between items-center py-4 px-6">
-            <p className="uppercase sm:font-semibold font-medium sm:text-sm text-xs dark:text-secondary-text text-dark">©2023 Hasini Punsala</p>
-            <p className="uppercase sm:font-semibold font-medium sm:text-sm text-xs dark:text-secondary-text text-dark">All Rights Reserved</p>
+        <footer className=" flex flex-col py-4 px-6 gap-4">
+            <hr></hr>
+            <div className="flex justify-between items-center">
+                <Image
+                    src='/images/Logo.svg'
+                    alt='logo'
+                    width={100}
+                    height={30}
+                />
+                <div className="gap-4">
+                    {NavLinks.map((link) => (
+                        <Link key={link.key} href={link.href} className='px-[15px] py-[6px] dark:text-light text-dark uppercase text-xs '>
+                            {link.text}
+                        </Link>
+                    ))}
+                </div>
+                <div className="flex flex-col">
+                    <p className="uppercase text-xs dark:text-secondary-text text-dark">©2023 Hasini Punsala</p>
+                    <p className="uppercase text-xs dark:text-secondary-text text-dark">All Rights Reserved</p>
+                </div>
+            </div>
         </footer>
     )
 }
