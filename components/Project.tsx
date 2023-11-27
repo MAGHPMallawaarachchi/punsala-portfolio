81dd3bfc6818dbe1081d8b80f6345558d3784574
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-27 22:31:19 +0530

    Add link and arrow icon to Project component

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
+import { BsArrowUpRightCircle } from "react-icons/bs";
 
 type Props = {
     name: string
@@ -14,16 +16,13 @@ const Project = ({name, category, description, link, image}: Props) => {
     <div className='flex flex-col gap-[20px] max-w-[520px]'>       
         <Image src={image} width={540} height={100} alt={name} className='rounded-2xl' />
     
-        <div className='flex flex-col items-start gap-1'>
-            <h4 className=' dark:text-secondary-text text-dark text-sm sm:text-lg font-medium'>{category}</h4>
-            <h1 className='dark:text-light text-dark sm:text-4xl text-3xl'>{name}</h1>
+        <div className='flex items-center justify-between'>
+          <div className='flex flex-col items-start gap-1'>
+              <h4 className=' dark:text-secondary-text text-dark text-sm sm:text-xl leading-none'>{category}</h4>
+              <h1 className='dark:text-light text-dark sm:text-[40px] tracking-[2px] text-2xl leading-none'>{name}</h1>
+          </div>
+          <Link href="/"><BsArrowUpRightCircle size="52px" className="dark:fill-light fill-dark sm:h-[52px] h-[36px]"/></Link>
         </div>
-        
-        <p className='text-dark dark:text-secondary-text text-base'>{description}</p>
-
-        <button className='border-2 border-dark rounded-full uppercase text-dark text-sm font-bold bg-blue w-40 py-2 sm:py-2'>
-            view project
-        </button>
     </div>
   )
 };
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,11 +11,11 @@ import { LuSparkle } from "react-icons/lu";
 const Projects = () => {
   return (
     <Container>
-      <section className='flex flex-col justify-center items-center' id='projects'>
+      <section className='flex flex-col justify-center items-center sm:gap-16 gap-10' id='projects'>
 
-        <div className='flex justify-between w-full'>
+        <div className='flex justify-between w-full items-end'>
 
-          <div className='flex items-center sm:mb-20 mb-16'>
+          <div className='flex items-center'>
             <div className='flex flex-col'>
               <div className='hidden lg:flex flex-col items-end'>
                 <LuSparkle size="72px" class="fill-blue stroke-[0.6px] stroke-dark -mb-6" color="#38B8FF"/>
@@ -34,7 +34,7 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className='sm:mt-20 mt-10 sm:flex hidden'>
+          <div className='sm:flex hidden'>
             <Link href='/projects'>
               <button className='button-text button bg-blue xsm:w-36 w-28 xsm:py-1 py-2'>View All</button>
             </Link>
@@ -42,7 +42,7 @@ const Projects = () => {
 
         </div>
         
-        <div className='flex flex-wrap gap-12 items-center lg:justify-between justify-center w-full'>
+        <div className='flex flex-wrap sm:gap-16 gap-10 items-center justify-center w-full'>
           {ProjectData.slice(0,4).map((project) => (
             <Project
               key={project.key}
@@ -55,7 +55,11 @@ const Projects = () => {
           ))}
         </div>
 
-          
+        <div className='sm:hidden flex'>
+          <Link href='/projects'>
+            <button className='button-text button bg-blue xsm:w-36 w-28 py-2'>View All</button>
+          </Link>
+        </div>
 
       </section>
     </Container>
