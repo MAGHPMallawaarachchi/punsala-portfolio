7db5f7f02932765cdabd6f2b9349e11cadb850c1
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-28 18:24:07 +0530

    Fix button styling transition duration

--- components/FilterButton.tsx
+++ components/FilterButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 const FilterButton = ({ text, onClick, active }: Props) => {
   return (
     <button
-      className={`uppercase  sm:text-base xsm:text-sm text-xs font-bold leading-none border-2 border-dark dark:border-light rounded-full sm:w-[160px] xsm:w-[100px] w-[95px] py-[5px] hover:dark:bg-light hover:dark:text-dark hover:text-light hover:bg-dark ${
+      className={`uppercase  sm:text-base xsm:text-sm text-xs font-bold leading-none border-2 border-dark dark:border-light rounded-full sm:w-[160px] xsm:w-[100px] w-[95px] py-[5px] hover:dark:bg-light hover:dark:text-dark hover:text-light hover:bg-dark transition-all duration-200 ${
         active
           ? 'dark:bg-light dark:text-dark bg-dark text-light'
           : 'text-dark dark:text-light'
