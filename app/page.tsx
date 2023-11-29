bcd3a6cde6a3116f7ee0abac8778f18b9f391b14
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-29 23:46:37 +0530

    Remove CNAME file and add Divider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from '@/components/About'
+import Divider from '@/components/Divider'
 import Hero from '@/components/Hero'
 import MyPassion from '@/components/MyPassion'
 import Projects from '@/components/Projects'
@@ -7,6 +8,7 @@ export default function Home() {
   return (
       <section>
         <Hero/>
+        <Divider/>
         <About/>
         <MyPassion/>
         <Projects />
