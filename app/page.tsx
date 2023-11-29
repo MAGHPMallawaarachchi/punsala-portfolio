dd9c90b670aa9a65e79d28587b1c48283956479f
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-29 22:21:50 +0530

    Remove Divider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import About from '@/components/About'
-import Divider from '@/components/Divider'
 import Hero from '@/components/Hero'
 import MyPassion from '@/components/MyPassion'
 import Projects from '@/components/Projects'
@@ -8,7 +7,6 @@ export default function Home() {
   return (
       <section>
         <Hero/>
-        <Divider/>
         <About/>
         <MyPassion/>
         <Projects />
